42143f40c9baf4b1d6cd4db2fb098e1ba3cfc64d
liukaijv <89016230@qq.com>
2016-10-28 11:23:01 +0800

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-#fanjin
+# xm-cms
+
+> php搭配smarty写的一个cms，用来快速建站。目录结构参照ecshop的
+
+## 运行
+
+修改data/config.php配置信息，导入根目录下的数据库文件
+
+	// database host
+	$db_host = "localhost";
+	// database name
+	$db_name = "xm-cms";
+	// database username
+	$db_user = "root";
+	// database password
+	$db_pass = "root";
--- index.php
+++ index.php
@@ -13,29 +13,5 @@ $keyword = isset($_GET['keyword']) ? urldecode($_GET['keyword']) : '';
 $site_title = $web->get_title($mid);
 $smarty->assign('site_title', $site_title);
 
-//服务QQ
-$service_qq = [];
-if (strpos($CFG['service_qq'], '|')) {
-    $groups = explode('|', $CFG['service_qq']);
-    if (is_array($groups)) {
-        foreach ($groups as $group) {
-            if (strpos($group, ',')) {
-                $service_qq[] = explode(',', $group);
-            }
-        }
-    }
-} else if (strpos($CFG['service_qq'], ',')) {
-    $service_qq[] = explode(',', $CFG['service_qq']);
-}
-$smarty->assign('service_qq', $service_qq);
-
 //首页
-if (!isset($mid) || empty($mid) || !is_int($mid) || $mid < 1) {    
-
-    $smarty->display('index.htm');
-
-} else {
-    
-    showMsg("出错了，您访问的页面不存在！");  
-
-}
+$smarty->display('index.htm');
